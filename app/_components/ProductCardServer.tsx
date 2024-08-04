31d38cb3b8763d250dfc21811708074d5551a1ed
ohiduzzamansiam1 <ohiduzzamansiam1@gmail.com>
2024-08-04 12:42:05 +0600

    Fixed auto login redirection from home page

--- app/_components/ProductCardServer.tsx
+++ app/_components/ProductCardServer.tsx
@@ -1,7 +1,6 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { Product } from "@prisma/client";
-import { redirect } from "next/navigation";
 import ProductCardClient from "./ProductCardClient";
 
 export default async function ProductCardServer({
@@ -11,12 +10,10 @@ export default async function ProductCardServer({
 }) {
   const { userId } = auth();
 
-  if (!userId) redirect("/sign-in");
-
   const isPurchased = !!(await db.order.findFirst({
     where: {
       productId: product.id,
-      userId,
+      userId: userId ?? "",
     },
   }));
 
